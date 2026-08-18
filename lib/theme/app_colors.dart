e371cdacec7fbf04432e513fc96c3dc172850124
2006nguyentrungnguyen-source <2006.nguyentrungnguyen@gmail.com>
2026-08-18 08:56:14 +0700

    Add files via upload

---
+++ lib/theme/app_colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Color(0xFF005B41);
+  static const Color primaryLight = Color(0xFFE8F5E9);
+  static const Color background = Color(0xFFF8F9FA);
+  static const Color cardBg = Color(0xFF005B41);
+  static const Color danger = Color(0xFFD32F2F);
+  static const Color textDark = Color(0xFF212121);
+  static const Color textMuted = Color(0xFF757575);
+}
